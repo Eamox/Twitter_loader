89dbe2c65324519edb488854a0d1d953f62ef5cb
Eamonn O'hanlon <eaamox@gmail.com>
2021-06-23 16:48:32 +1000

    t

--- vis.js
+++ vis.js
@@ -1,24 +1,37 @@
 looker.plugins.visualizations.add({
-  create: function(element, config) {
+    create: function(element, config) {
+	var twit = document.createElement('script')
+      twit.innerHTML=`window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));`
+	element.appendChild(twit)
+	var container = document.createElement('div')
+	container.id="tweet-container"
+	element.appendChild(container)
+	console.log(twttr)
 
   },
   updateAsync: function(data, element, config, queryResponse, details, done) {
 
     post = data[0][queryResponse.fields.dimensions[0].name].value;
-
-    var block;
-    if (block = document.getElementsByTagName('iframe')[0]) {
-      var current = block.getAttribute('src')
-	if (current.includes(post)) {
-        return true
-      }
-    };
-    var body = document.getElementsByTagName('body')[0];
-    body.innerHTML = "";
-    var di = document.createElement('div');
-    di.innerHTML = `<iframe border=0 frameborder=0 height=250 width=550
- src="https://twitframe.com/show?url=https://twitter.com/Australia/status/${post}"></iframe> 
-`;
-    body.appendChild(di);
-  }
-})
+     
+      
+      twttr.ready(function(){
+	  console.log('loaded');
+	  twttr.widgets.createTweet(post,document.getElementById('tweet-container')).then(function(el){console.log(el)})
+      })
+						
+  }})
